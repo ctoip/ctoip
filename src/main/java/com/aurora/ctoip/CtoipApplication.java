16a1464367b7bfcf8b4f0bde84eea0674605bb7c
Dew-OF-Aurora <1962662993@qq.com>
2023-03-28 06:06:42 +0800

    3.28

--- src/main/java/com/aurora/ctoip/CtoipApplication.java
+++ src/main/java/com/aurora/ctoip/CtoipApplication.java
@@ -1,13 +1,16 @@
 package com.aurora.ctoip;
 
+import com.aurora.ctoip.controller.BaseController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ServletComponentScan("com.aurora.ctoip.common.filter")
 @EnableTransactionManagement
+@EnableConfigurationProperties(BaseController.class)
 public class CtoipApplication {
     public static void main(String[] args) {
         SpringApplication.run(CtoipApplication.class, args);
--- src/main/java/com/aurora/ctoip/controller/BaseController.java
+++ src/main/java/com/aurora/ctoip/controller/BaseController.java
@@ -3,6 +3,9 @@ package com.aurora.ctoip.controller;
 import com.aurora.ctoip.service.SysUserService;
 import com.aurora.ctoip.util.RedisUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -14,24 +17,23 @@ import javax.servlet.http.HttpServletResponse;
  * @create: 2023-02-21 00:24
  * @Description: 通用控制器
  */
+@Data
+@Component
+@ConfigurationProperties(prefix = "aurora.apikey")
 public class BaseController {
+    public String threatBookApiKey;
+    public String VTApiKey;
     @Resource
     HttpServletRequest request;
-
     @Resource
     HttpServletResponse response;
-
     @Resource
     RestTemplate restTemplate;
-
     @Resource
     RedisUtil redisUtil;
-
     @Resource
     SysUserService sysUserService;
-
     @Resource
     ObjectMapper objectMapper;
-
 
 }
